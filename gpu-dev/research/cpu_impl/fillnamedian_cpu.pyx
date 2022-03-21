b9bd666bf5d6aee5688315f2d0f9dfbef0c3664a
egor <ekserviceek@gmail.com>
2022-03-21 14:24:11 +0300

    removed extra files

--- gpu-dev/research/cpu_impl/fillnamedian_cpu.pyx
+++
@@ -1,7 +0,0 @@
-cimport numpy as np
-
-cdef extern from "dev_bucket_sort.hh":
-    void fill_na_median_cpu(double * d_v, size_t N)
-    
-def fillnamedian_cpu(np.ndarray[ndim=1, dtype=np.float64_t] d_v, N):
-    fill_na_median_cpu(&d_v[0], N)
