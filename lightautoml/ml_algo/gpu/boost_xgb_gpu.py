d3061b1fb1edeb763db6ac7cdc138b924bc531a6
egor <ekserviceek@gmail.com>
2022-04-07 14:19:36 +0300

    added dataset conversion to xgb

--- lightautoml/ml_algo/gpu/boost_xgb_gpu.py
+++ lightautoml/ml_algo/gpu/boost_xgb_gpu.py
@@ -377,6 +377,9 @@ class BoostXGB_dask(BoostXGB):
 
         print("train data is", type(train.data))
         print("dataset is", type(train))
+        if type(train) is not DaskCudfDataset:
+            train.data = train.data.to_daskcudf(nparts=2)
+            valid.data = valid.data.to_daskcudf(nparts=2)
 
         xgb_train = dxgb.DaskDeviceQuantileDMatrix(self.client, train.data, label=train_target, weight=train_weight)
         xgb_valid = dxgb.DaskDeviceQuantileDMatrix(self.client, valid.data, label=valid_target, weight=valid_weight)
